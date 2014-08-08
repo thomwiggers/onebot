575cf2f5f49aab9b7a35419816d829f657f94e94
Thom Wiggers <thom@thomwiggers.nl>
2014-08-08 19:49:18 +0200

    fix testing

--- setup.py
+++ setup.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# flake8: noqa
+from __future__ import unicode_literals, print_function
 
 import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
 
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 install_requires = ['irc3', 'wheel', 'lfmh']
+test_requires = ['pytest']
 
 if sys.version_info < (3, 4):
     install_requires.append('trollius')
+if sys.version_info < (3, 3):
+    test_requires.append('mock')
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
 
 setup(
     name='onebot',
@@ -24,16 +41,14 @@ setup(
     author='Thom Wiggers',
     author_email='thom@thomwiggers.nl',
     url='https://github.com/thomwiggers/onebot',
-    packages=[
-        'onebot',
-    ],
-    package_dir={'onebot':
-                 'onebot'},
-    include_package_data=True,
+    packages=find_packages(
+        exclude=['*.tests', 'tests', 'tests.*', '*.tests.*']
+    ),
+    include_package_data=False,
     install_requires=install_requires,
-    license="BSD",
-    zip_safe=False,
-    keywords='onebot',
+    license='BSD',
+    zip_safe=True,
+    keywords='onebot irc',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -44,12 +59,14 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    test_suite='tests',
-    tests_require=[
-    ],
-    entry_points='''
-    [console_scripts]
-    onebot = onebot:run
-    ''',
+    tests_require=test_requires,
+    cmdclass={'test': PyTest},
+    entry_points={
+        'console_scripts': [
+            'onebot = onebot:run',
+        ]
+    }
 )
