066e9f6bc37dba3b8a9fef51edcfd62041f4c54c
Thom Wiggers <thom@thomwiggers.nl>
2015-08-15 13:54:52 +0200

    Add restart command (Closes #6)
    Also use bot instead of _bot

--- onebot/plugins/botui.py
+++ onebot/plugins/botui.py
@@ -7,7 +7,6 @@ Based on http://git.io/v3HVL by gawel
 
 from irc3.plugins.command import command
 from irc3 import plugin, event
-import logging
 
 
 @plugin
@@ -16,18 +15,9 @@ class BotUI(object):
 
     def __init__(self, bot):
         """Init"""
-        self._bot = bot
+        self.bot = bot
         self._config = bot.config.get(__name__, {})
         self._log = self.bot.log.getChild(__name__)
-
-        if self._bot.config.get('verbose'):
-            self._log.setLevel(logging.DEBUG)
-        else:
-            level = self._bot.config.get('level')
-
-            if level is not None:
-                level = getattr(logging, str(level), level)
-                self._log.setLevel(level)
 
         self._autojoin = self._config.get('joininvite', False)
         self._admin = self._config.get('admin', '')
@@ -40,16 +30,16 @@ class BotUI(object):
         self._log.info("%s invited me to %s." % (sender, channel))
 
         if self._autojoin:
-            if target == self._bot.nick:
-                self._bot.join(channel)
+            if target == self.bot.nick:
+                self.bot.join(channel)
         else:
             self.bot.privmsg(
                 sender,
                 "Never accept an invitation from a stranger unless he gives "
                 "you candy. -- Linda Festa")
             if self._admin:
-                self._bot.notice(self._admin,
-                                 "%s invited me to %s." % (sender, channel))
+                self.bot.notice(self._admin,
+                                "%s invited me to %s." % (sender, channel))
 
     @command(permission="operator")
     def join(self, mask, target, args):
@@ -63,7 +53,7 @@ class BotUI(object):
         if args['<password>'] is not None:
             channel += " %s" % args['<password>']
 
-        self._bot.join(channel)
+        self.bot.join(channel)
 
     @command(permission="operator")
     def part(self, mask, target, args):
@@ -75,7 +65,7 @@ class BotUI(object):
         if args['<channel>'] is not None:
             target = args['<channel>']
 
-        self._bot.part(target)
+        self.bot.part(target)
 
     @command(permission='admin')
     def quit(self, mask, target, args):
@@ -84,8 +74,8 @@ class BotUI(object):
         %%quit [<reason>]
         """
 
-        self._bot.quit(args['<reason>'])
-        self._bot.loop.stop()
+        self.bot.quit((args['<reason>'] or '') + ' -- {}'.format(mask.nick))
+        self.bot.loop.stop()
 
     @command(permission='admin')
     def nick(self, mask, target, args):
@@ -94,7 +84,7 @@ class BotUI(object):
         %%nick <nick>
         """
 
-        self._bot.set_nick(args['<nick>'])
+        self.bot.set_nick(args['<nick>'])
 
     @command(permission='operator')
     def mode(self, mask, target, args):
@@ -104,7 +94,7 @@ class BotUI(object):
         %%mode <mode cmd>
         """
 
-        self._bot.mode(self._bot.nick, args['<mode cmd>'])
+        self.bot.mode(self.bot.nick, args['<mode cmd>'])
 
     @command(permission='admin')
     def msg(self, mask, target, args):
@@ -114,7 +104,7 @@ class BotUI(object):
         %%msg <target> <message>
         """
 
-        self._bot.privmsg(args['<target>'], args['<message>'])
+        self.bot.privmsg(args['<target>'], args['<message>'])
 
     @command(permission='admin')
     def quote(self, mask, target, args):
@@ -122,4 +112,15 @@ class BotUI(object):
 
         %%quote <string>
         """
-        self._bot.send(args['<string>'])
+        self.bot.send(args['<string>'])
+
+    @command(permission='admin')
+    def restart(self, mask, target, args):
+        """Quit the IRC bot with an error code to signal restart
+
+          %%restart [<reason>]
+        """
+        import sys
+        self.bot.quit((args['<reason>'] or '') + ' -- {}'.format(mask.nick))
+        self.bot.loop.stop()
+        sys.exit(2)
