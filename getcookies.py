102a3740ac572a730216bc17dc1ceb8f76f84897
Thom Wiggers <thom@thomwiggers.nl>
2015-08-30 19:47:16 +0200

    make this file safe for test collection

--- getcookies.py
+++ getcookies.py
@@ -2,14 +2,16 @@ import requests
 import pickle
 import json
 
-with open('sites.json', 'r') as f:
-    sites = json.load(f)
 
-for url, data in sites.items():
-    with requests.Session() as session:
-        result = session.post(url, data=data)
-        if result.ok:
-            print("logged in to {}".format(url))
+if __name__ == "__main__":
+    with open('sites.json', 'r') as f:
+        sites = json.load(f)
 
-with open('cookies.jar', 'wb') as f:
-    pickle.dump(session.cookies, f)
+    for url, data in sites.items():
+        with requests.Session() as session:
+            result = session.post(url, data=data)
+            if result.ok:
+                print("logged in to {}".format(url))
+
+    with open('cookies.jar', 'wb') as f:
+        pickle.dump(session.cookies, f)
