19c23ef43a2230112ad0d03918694147b2ddb51a
Thom Wiggers <thom@thomwiggers.nl>
2015-09-04 23:45:16 +0200

    make messages more conservative, prevent some DOS

--- onebot/plugins/urlinfo.py
+++ onebot/plugins/urlinfo.py
@@ -32,6 +32,9 @@ class UrlInfo(object):
         self.config = bot.config.get(__name__, {})
         self.log = self.bot.log.getChild(__name__)
         cookiejar_file = self.config.get('cookiejar')
+        self.ignored_classes = self.config.get('ignored_classes',
+                                               ['image', 'text'])
+        self.ignored_apps = self.config.get('ignored_apps', ['pdf'])
         if cookiejar_file:
             with open(cookiejar_file, 'rb') as f:
                 self.cookiejar = pickle.load(f)
@@ -46,7 +49,7 @@ class UrlInfo(object):
         index = 1
         urls = re.findall(r'https?://\S+', data)
         for url in urls:
-            message = ["URL:"]
+            message = []
             o = urlparse(url)
             if o.hostname in ('127.0.0.1', '[::1]',
                               'localhost',
@@ -57,7 +60,7 @@ class UrlInfo(object):
                 url = url.replace(o.hostname, self.urlmap[o.hostname], 1)
                 message.append(url)
             with requests.Session() as session:
-                session.headers.update({'User-Agent': "requests/OneBot"})
+                session.headers.update({'User-Agent': "linux:onebot:1"})
                 session.cookies = self.cookiejar
                 self.log.debug("processing %s", url)
                 if len(urls) > 1:
@@ -68,26 +71,30 @@ class UrlInfo(object):
                         url, allow_redirects=True, timeout=3)
                     content_type = response.headers.get(
                         'Content-Type', 'text/html').split(';')[0]
+                    size = int(response.headers['Content-Length'])
                     if not response.ok:
                         message.append("error:")
                         message.append(response.reason.lower())
-                    elif content_type not in (
-                            'text/html', 'application/xhtml+xml'):
-                        message.append("type:")
+                    elif (content_type not in (
+                            'text/html', 'application/xhtml+xml')
+                            and size < (1048576 * 5)):
+                        class_, app = content_type.split('/')
+                        if ((class_ in self.ignored_classes
+                                or app in self.ignored_apps)
+                                and size < (1048576 * 5)):
+                            return
+                        message.append("Content-Type:")
                         message.append(response.headers['Content-Type'])
-                        message.append("size:")
-                        message.append(sizeof_fmt(
-                            int(response.headers['Content-Length'])))
+                        message.append("Filesize:")
+                        message.append(sizeof_fmt(size))
                     else:
                         result = session.get(
                             url, allow_redirects=True, timeout=4)
                         soup = BeautifulSoup(result.content, 'html.parser')
                         if hasattr(soup, 'title'):
-                            message.append("title:")
                             message.append(
                                 "“{}”".format(soup.title.string.strip()))
                 except requests.exceptions.Timeout:
-                    message.append("error: timeout")
                     self.log.debug("Error while requesting %s", url)
                 except:
                     self.log.exception(
