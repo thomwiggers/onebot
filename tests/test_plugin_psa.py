d910d9e4af201a773e1f203ceb7a64ce287a0d9a
Mattbox <mattbox@users.noreply.github.com>
2016-11-08 01:47:29 -0600

    Simplification

--- tests/test_plugin_psa.py
+++ tests/test_plugin_psa.py
@@ -20,27 +20,19 @@ class PSATestCase(BotTestCase):
             'onebot.plugins.psa',
             'irc3.plugins.command'
         ],
-        'autojoins': [
-            '${hash}chan1',
-            '${hash}chan2'
-        ],
         'cmd': '!'
     }
 
     @patch('irc3.plugins.storage.Storage')
     def setUp(self, mock):
         super(PSATestCase, self).setUp()
-        self.callFTU(nick='one')
+        self.callFTU()
         self.bot.db = MockDb({
-            'wat@bro': {'permissions': {'all_permissions'}}
+            'the@boss': {'permissions': {'all_permissions'}}
         })
 
-    def test_more(self):
-        plugin = self.bot.get_plugin('irc3.plugins.userlist.Userlist')
-        self.assertIn('one', plugin.channels['#chan1'])
-        self.assertIn('one', plugin.channels['#chan2'])
-
     def test_psa(self):
-        self.bot.dispatch(':im!wat@bro PRIVMSG #chan1 :!psa foo')
-        self.assertSent(['PRIVMSG #chan1 :foo'])
-        self.assertSent(['PRIVMSG #chan2 :foo'])
+        self.bot.dispatch('JOIN #chan1')
+        self.bot.dispatch('JOIN #chan2')
+        self.bot.dispatch(':im!the@boss PRIVMSG #chan1 :!psa foo bar')
+        self.assertSent(['PRIVMSG #chan1 :foo bar','PRIVMSG #chan2 :foo bar'])
