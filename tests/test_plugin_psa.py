12c07b6d689c6c36a36035f462d377988e7876c1
Mattbox <mattbox@users.noreply.github.com>
2016-11-07 22:44:14 -0600

    Test for PSA module

---
+++ tests/test_plugin_psa.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+test_plugin_psa
+----------------------------------
+
+Tests for PSA module
+"""
+from irc3.testing import BotTestCase, patch
+
+from .test_plugin_users import MockDb
+
+
+class PSATestCase(BotTestCase):
+
+    config = {
+        'includes': [
+            'onebot.plugins.psa',
+            'irc3.plugins.command',
+        ],
+        'cmd': '!'
+    }
+
+    @patch('irc3.plugins.storage.Storage')
+    def setUp(self, mock):
+        super(PSATestCase, self).setUp()
+        self.callFTU()
+        self.bot.db = MockDb({
+            'the@boss': {'permissions': {'all_permissions'}}
+        })
+
+    def test_acl(self):
+        self.bot.dispatch(':im!the@boss PRIVMSG #chan :!acl best bot')
+        for chan in self.bot.channels:
+            self.assertSent(['PRIVMSG {} :best bot'.format(chan)])
